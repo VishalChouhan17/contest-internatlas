46e3598025925e552f309be76a672d39f1342948
VishalChouhan17 <vishal.chouhan2028@gmail.com>
2026-09-17 15:38:01 +0530

    404 for GET/contests fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ContestsModule } from './contests/contests.module.js';
 
 @Module({
-  imports: [],
+  imports: [ContestsModule],
   controllers: [AppController],
   providers: [AppService],
 })
