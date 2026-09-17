afca11985fa8b92ac35c702a1674e79dbb56cb32
Vishal Chouhan <vishalchouhan9009280654@gmail.com>
2026-09-17 15:55:03 +0530

    fix: register global ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ContestsModule } from './contests/contests.module.js';
 
 @Module({
-  imports: [ContestsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ContestsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
